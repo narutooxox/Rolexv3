d5a5152ff545e9dbd1d92e9af04a362918557e3d
narutooxox <112558957+narutooxox@users.noreply.github.com>
2022-09-05 00:44:34 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,59 +1,66 @@
 class script(object):
-    START_TXT = """𝙃𝙚𝙡𝙡𝙤 {},
-𝙈𝙮 𝙉𝙖𝙢𝙚 𝙄𝙨 <a href=https://t.me/{}>{}</a>, 𝙄 𝘾𝙖𝙣 𝙋𝙧𝙤𝙫𝙞𝙙𝙚 𝙈𝙤𝙫𝙞𝙚𝙨 𝙏𝙤 𝙔𝙤𝙪𝙧 𝘾𝙝𝙖𝙣𝙣𝙚𝙡 𝙏𝙤 𝙔𝙤𝙪𝙧 𝙂𝙧𝙤𝙪𝙥 𝘼𝙡𝙨𝙤, 𝙈𝙖𝙠𝙚 𝙈𝙚 𝘼𝙨 𝘼𝙙𝙢𝙞𝙣 𝙄𝙣 𝙔𝙤𝙪𝙧 𝙂𝙧𝙤𝙪𝙥 𝙩𝙤 𝙎𝙚𝙧𝙫𝙚 𝙈𝙤𝙫𝙞𝙚𝙨 𝘿𝙤𝙣𝙚 😁👍🏻"""
-    HELP_TXT = """𝙃𝙚𝙮 {}
-𝙃𝙚𝙧𝙚 𝙄𝙨 𝙏𝙝𝙚 𝙃𝙚𝙡𝙥 𝙁𝙤𝙧 𝙈𝙮 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨."""
-    ABOUT_TXT = """» 𝙈𝙮 𝙉𝙖𝙢𝙚: {}
-» 𝘾𝙧𝙚𝙖𝙩𝙤𝙧: <a href=https://t.me/RolexMoviesOX>𝙍𝙤𝙡𝙚𝙭 𝘾𝙤𝙢𝙢𝙪𝙣𝙞𝙩𝙮</a>
-» 𝙇𝙖𝙣𝙜𝙪𝙖𝙜𝙚: 𝙋𝙔𝙏𝙃𝙊𝙉 
-» 𝘿𝙖𝙩𝙖 𝘽𝙖𝙨𝙚: 𝙈𝙊𝙉𝙂𝙊 𝘿𝘽
-» 𝘽𝙤𝙩 𝙎𝙚𝙧𝙫𝙚𝙧: 𝙃𝙀𝙍𝙊𝙆𝙐"""
-    SOURCE_TXT = """<b>Details:</b>
+    START_TXT = """<b> Hello {} 👋🏻 Im Rolex Bot I can share Movies and Series 😁.</b>
     
-- Anybody Wants This Bot Contact Me @RolexContact_bot"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
+<i>Add me to your Group and make me as Admin to share files in your Group</i>"""
+    HELP_TXT = """Hey {}
+Here Is The Help For MY Commands."""
+    ABOUT_TXT = """<b><i>🧸 Bot Name : <a href=https://t.me/RolexMoviesOX><b>Rolex Bot</b></a>\n
+😎 Owner : <a href=https://t.me/JonSnow11><b>Jon Snow</b></a>\n
+📡 Host : ʜᴇʀᴏᴋᴜ\n
+🌟 Version : ᴠ 4.0 [ ʙᴇᴛᴀ ]\n</b></i>"""
+    SOURCE_TXT = """<b>𝐂𝐫𝐞𝐚𝐭𝐞 𝐎𝐧𝐞 𝐋𝐢𝐤𝐞 𝐓𝐡𝐢𝐬:</b>
+» First Share Me Your Heroku Api
+» I will Create One Bot For You<b>
+» Contact Me @JonSnow11<b>"""
 
-<b>Commands:</b>
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
+- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+<b>NOTE:</b>
+1. eva maria should have admin privillage.
+2. only admins can add filters in a chat.
+3. alert buttons have a limit of 64 characters.
+<b>Commands and Usage:</b>
 • /filter - <code>add a filter in chat</code>
 • /filters - <code>list all the filters of a chat</code>
 • /del - <code>delete a specific filter in chat</code>
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
-
-<b>Button Commands:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>"""
+- Eva Maria Supports both url and alert inline buttons.
+<b>NOTE:</b>
+1. Telegram will not allows you to send buttons without any content, so content is mandatory.
+2. Eva Maria supports buttons with any telegram media type.
+3. Buttons should be properly parsed as markdown format
+<b>URL buttons:</b>
+<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<b>Alert buttons:</b>
+<code>[Button Text](buttonalert:This is an alert message)</code>"""
     AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
-
-<b>Details:</b>
+<b>NOTE:</b>
 1. Make me the admin of your channel if it's private.
 2. make sure that your channel does not contains camrips, porn and fake files.
 3. Forward the last message to me with quotes.
  I'll add all the files in that channel to my db."""
     CONNECTION_TXT = """Help: <b>Connections</b>
-
 - Used to connect bot to PM for managing filters 
 - it helps to avoid spamming in groups.
-
 <b>NOTE:</b>
 1. Only admins can add a connection.
 2. Send <code>/connect</code> for connecting me to ur PM
-
 <b>Commands and Usage:</b>
 • /connect  - <code>connect a particular chat to your PM</code>
 • /disconnect  - <code>disconnect from a chat</code>
 • /connections - <code>list all your connections</code>"""
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
-
-<b>Commands</b>
+<b>NOTE:</b>
+these are the extra features of Eva Maria
+<b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
 • /info  - <code>get information about a user.</code>
 • /imdb  - <code>get the film information from IMDb source.</code>
 • /search  - <code>get the film information from various sources.</code>"""
     ADMIN_TXT = """Help: <b>Admin mods</b>
-
 <b>NOTE:</b>
 This module only works for my admins
-
 <b>Commands and Usage:</b>
 • /logs - <code>to get the rescent errors</code>
 • /stats - <code>to get status of files in db.</code>
@@ -66,11 +73,13 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+    STATUS_TXT = """<b><i>Stats</b></i>
+    
+⁕ Total Files: <code>{}</code>
+⁕ Total Users: <code>{}</code>
+⁕ Total Groups: <code>{}</code>
+⁕ Storage: <code>{}</code>
+"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
